f4488ce6873496edb7f46e7a85087996b2429d5a
Bibhu Pandey <pandeybibhu89@outlook.com>
2025-09-07 01:16:32 +0530

    Updated test class file

--- cypress/e2e/HRM_TC_AdminDasgboard.cy.js
+++ cypress/e2e/HRM_TC_AdminDasgboard.cy.js
@@ -8,43 +8,43 @@ describe("Admin Dashboard", () => {
   });
   afterEach(() => {
     loginPageActions.verifyUserProfile();
-    // loginPageActions.selectLogoutButton();
+    loginPageActions.selectLogoutButton();
   });
-  it.skip("Navigate to Admin Dashboard", () => {
+  it("Navigate to Admin Dashboard", () => {
     cy.wait(5000);
     hrm_AdminPageActions.clickOnAdminDashboard();
     // hrm_AdminPageActions.checkBreadCumPathAtAdmin();
   });
-  it.skip("Verify Admin/UserManagement breadcum path", () => {
+  it("Verify Admin/UserManagement breadcum path", () => {
     cy.wait(5000);
     hrm_AdminPageActions.clickOnAdminDashboard();
     hrm_AdminPageActions.checkBreadCumPathAtAdmin();
   });
-  it.skip("Verify job Dropdown item list", () => {
+  it("Verify job Dropdown item list", () => {
     cy.wait(5000);
     hrm_AdminPageActions.clickOnAdminDashboard();
     hrm_AdminPageActions.checkBreadCumPathAtAdmin();
     hrm_AdminPageActions.checkJobMenuDropDownList("Pay Grades");
   });
-  it.skip("Verify Admin Organisaton Dropdown item list", () => {
+  it("Verify Admin Organisaton Dropdown item list", () => {
     cy.wait(5000);
     hrm_AdminPageActions.clickOnAdminDashboard();
     hrm_AdminPageActions.checkBreadCumPathAtAdmin();
     hrm_AdminPageActions.checkOrganisationMenuDropDownList("Locations");
   });
-  it.skip("Verify Admin Qualifications Dropdown item list", () => {
+  it("Verify Admin Qualifications Dropdown item list", () => {
     cy.wait(5000);
     hrm_AdminPageActions.clickOnAdminDashboard();
     hrm_AdminPageActions.checkBreadCumPathAtAdmin();
     hrm_AdminPageActions.checkQualificationMenuDropdownList("Languages");
   });
-  it.skip("Verify Add Skills", () => {
+  it("Verify Add Skills", () => {
     hrm_AdminPageActions.clickOnAdminDashboard();
     hrm_AdminPageActions.checkQualificationMenuDropdownList("Skills");
     hrm_AdminPageActions.addSkills("CypressAutomatioadfdfdfsdasdn","Automation with Cypress");
     hrm_AdminPageActions.verifyToastORSuccessfullAlert();
   });
-  it("Verify delete skills",()=>{
+  it.skip("Verify delete skills",()=>{
     hrm_AdminPageActions.clickOnAdminDashboard();
     hrm_AdminPageActions.checkQualificationMenuDropdownList("Skills");
     hrm_AdminPageActions.addSkills("sdsdsdsdsdsdsdsdsdsd","Automation with Cypress");
